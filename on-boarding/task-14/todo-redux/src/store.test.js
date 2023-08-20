a9d6a4787d5a760077ae37b19d27ba8ed0d80ae8
Naol Taye <naoltd164@gmail.com>
2023-08-20 16:26:18 +0300

    final-commit

--- on-boarding/task-14/todo-redux/src/store.test.js
+++ on-boarding/task-14/todo-redux/src/store.test.js
@@ -1,30 +1,82 @@
-import configureMockStore from 'redux-mock-store';
-import tasksReducer from './reducers/taskSlice';
-import store from './store';
+describe('End-to-End Tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
-const mockStore = configureMockStore([]);
+  it('should add a new task and display it in the task list', () => {
+    const taskText = 'New Task';
 
-describe('Store Configuration', () => {
-  test('should configure the store correctly', () => {
-    const initialState = { tasks: { tasks: [], filter: false } };
-    const expectedActions = [];
-    const expectedState = { tasks: [] };
+    // Type a task in the input field
+    cy.get('input[type="text"]').type(taskText);
 
-    const store = mockStore(initialState);
-    store.replaceReducer(tasksReducer);
+    // Click the "Add Todo" button
+    cy.get('button[type="submit"]').click();
 
-    const actions = store.getActions();
-    const state = store.getState();
+    // Verify that the new task is added to the list
+    cy.contains('.task-item', taskText);
+  });
 
-    expect(actions).toEqual(expectedActions);
-    expect(state).toEqual(expectedState);
+  it('should toggle the task filter when the filter button is clicked', () => {
+    // Click the filter button to toggle the filter
+    cy.get('.filterbutton').click();
+
+    // Verify that the filter has changed to show completed tasks
+    cy.contains('.filterbutton', 'Show All Tasks').should('be.visible');
+
+    // Click the filter button again to toggle the filter back
+    cy.get('.filterbutton').click();
+
+    // Verify that the filter has changed back to show all tasks
+    cy.contains('.filterbutton', 'Show Completed Tasks').should('be.visible');
+  });
+
+  it('should delete a task when the delete button is clicked', () => {
+    // Add a new task
+    const taskText = 'Task to Delete';
+    cy.get('input[type="text"]').type(taskText);
+    cy.get('button[type="submit"]').click();
+
+    // Find the newly added task and click the delete button
+    cy.contains('.task-item', taskText)
+      .find('.delete-button')
+      .click();
+
+    // Verify that the task is deleted and not visible in the task list
+    cy.contains('.task-item', taskText).should('not.exist');
+  });
+
+  it('should toggle the completed status of a task when the checkbox is clicked', () => {
+    // Add a new task
+    const taskText = 'Task to Toggle';
+    cy.get('input[type="text"]').type(taskText);
+    cy.get('button[type="submit"]').click();
+
+    // Find the newly added task and click the checkbox
+    cy.contains('.task-item', taskText)
+      .find('input[type="checkbox"]')
+      .click();
+
+    // Verify that the task's completed status is toggled
+    cy.contains('.task-item.completed', taskText).should('be.visible');
+  });
+
+  it('should edit a task when the edit button is clicked', () => {
+    // Add a new task
+    const initialTaskText = 'Task to Edit';
+    const updatedTaskText = 'Updated Task';
+    cy.get('input[type="text"]').type(initialTaskText);
+    cy.get('button[type="submit"]').click();
+
+    // Find the newly added task and click the edit button
+    cy.contains('.task-item', initialTaskText)
+      .find('.edit-button')
+      .click();
+
+    // Update the task text in the input field and submit the form
+    cy.get('input[type="text"]').clear().type(updatedTaskText);
+    cy.get('button[type="submit"]').click();
+
+    // Verify that the task text is updated in the task list
+    cy.contains('.task-item', updatedTaskText).should('be.visible');
   });
 });
-
-
-// In this test, we're using the redux-mock-store library to create a mock store 
-// with the initial state. We then replace the reducer with the tasksReducer from your code.
-
-// The test verifies that the store is configured correctly by comparing the dispatched 
-// actions (actions) and the resulting state (state) with the expected values 
-// (expectedActions and expectedState, respectively).
